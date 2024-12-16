1dd250edd7a457dc0b34b518a9e9a331e4290e2a
Track404 <111713000+Track404@users.noreply.github.com>
2024-12-16 23:33:36 +0100

    make the logic of game work (compare array to id)

--- src/Card.jsx
+++ src/Card.jsx
@@ -2,25 +2,30 @@ import { useEffect } from 'react';
 import { useState } from 'react';
 /* eslint-disable react/prop-types */
 
-export default function Card({ url, name }) {
+export default function Card({ url, name, onClick }) {
   const [data, setData] = useState(null);
+
   useEffect(() => {
     fetch(url, { mode: 'cors' })
       .then((response) => response.json())
-      .then((json) => setData(json.sprites.front_default));
+      .then((json) => setData(json));
   }, [url]);
+
   return (
     data && (
       <>
-        <div className="Card">
+        <div id={data.id} className="Card" onClick={onClick}>
           <h2>{name}</h2>
-          <img src={data} alt="PokemonImage" />
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit
-            quisquam dignissimos accusantium cumque sunt impedit quibusdam velit
-            mollitia asperiores consequatur praesentium reprehenderit recusandae
-            aperiam porro, ducimus dolores doloremque? Minima, ab!
-          </p>
+          <img src={data.sprites.front_default} alt="PokemonImage" />
+
+          <div>
+            <h3>Abilities :</h3>
+            <ul>
+              {data.abilities.map((info) => {
+                return <li key={crypto.randomUUID()}>{info.ability.name}</li>;
+              })}
+            </ul>
+          </div>
         </div>
       </>
     )
--- src/Game.jsx
+++ src/Game.jsx
@@ -1,6 +1,11 @@
 import { useEffect, useState } from 'react';
 import Card from './Card';
 export default function Game() {
+  const [topScore, setTopScore] = useState(0);
+  const [currentScore, setCurrentScore] = useState(0);
+
+  const [clickCardName, setClickCardName] = useState([]);
+
   const [data, setData] = useState(null);
   useEffect(() => {
     fetch('https://pokeapi.co/api/v2/pokemon/?limit=12', { mode: 'cors' })
@@ -9,15 +14,42 @@ export default function Game() {
       .catch((error) => console.error(error));
   }, []);
 
+  function cardClick(e) {
+    let theId = e.currentTarget.id;
+    if (clickCardName.includes(theId)) {
+      setCurrentScore(0);
+      setClickCardName([]);
+    } else {
+      setCurrentScore(currentScore + 1);
+      setClickCardName([...clickCardName, theId]);
+    }
+  }
   return (
     data && (
-      <div className="containerCard">
-        {data.results.map((info) => {
-          return (
-            <Card key={crypto.randomUUID()} name={info.name} url={info.url} />
-          );
-        })}
-      </div>
+      <>
+        <div className="scoreBoard">
+          <div>
+            <h2>Top Score</h2>
+            <p>{topScore}</p>
+          </div>
+          <div>
+            <h2>Current Score</h2>
+            <p>{currentScore}</p>
+          </div>
+        </div>
+        <div className="containerCard">
+          {data.results.map((info) => {
+            return (
+              <Card
+                key={info.name}
+                name={info.name}
+                url={info.url}
+                onClick={cardClick}
+              />
+            );
+          })}
+        </div>
+      </>
     )
   );
 }
